6b5961a2c9f44b28b62f6e7e57a317b1b92041bf
devMuromi <devmuromi@gmail.com>
2023-07-18 16:06:33 +0900

    2. Book, Author 모델 추가

--- books/models.py
+++ books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    age = models.IntegerField(null=True)
+
+
+class Book(models.Model):
+    title = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    page_count = models.IntegerField(null=True)
