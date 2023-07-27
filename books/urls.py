d0e41a4dc6d67ba1f5e821a84daecf416c588b64
devMuromi <devmuromi@gmail.com>
2023-07-27 11:44:16 +0900

    4. view 사용법

--- books/urls.py
+++ books/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from books import views
 
-urlpatterns = [path("books/", views.books, name="books"), path("books/<int:book_id>", views.book)]
+urlpatterns = [
+    path("", views.books, name="books"),
+    path("<int:book_id>/", views.book_detail, name="book_detail"),
+    path("create/", views.create_book, name="create_book"),
+]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from books.models import Book
 
-# Create your views here.
+
+def books(request):
+    books = Book.objects.all()
+    res = ""
+    for book in books:
+        res += f"{book.title} / {book.author.name}<br>"
+    return HttpResponse(res)
+
+
+def book_detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+    return HttpResponse(f"{book.title} / {book.author.name}")
+
+
+def create_book(request):
+    Book.objects.create(title="빙과", page_count=256, author_id=1)
+    return HttpResponse("책 생성 완료")
